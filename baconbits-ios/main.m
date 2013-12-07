088c8d770ce595387520f889a13a1c83171f65a4
Greg Williams <williams@atomicembedded.com>
2013-12-07 11:30:02 -0500

    Initial Commit

---
+++ baconbits-ios/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  baconbits-ios
+//
+//  Created by Greg Williams on 12/7/13.
+//  Copyright Atomic Object LLC 2013. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
